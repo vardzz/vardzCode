ef8e253a9d2af946d3b9faf5f3398ff07ed07929
Jericho Varde <vardejericho@gmail.com>
2026-03-23 14:35:39 +0800

    feat: added projects image

--- src/components/custom/projects.jsx
+++ src/components/custom/projects.jsx
@@ -18,19 +18,19 @@ export default function Projects() {
       id: "02.",
       title: "HORIZON AI", 
       desc: "A studio focused on making complex AI data tangible and visually breathtaking for engineers.",
-      img: "https://lh3.googleusercontent.com/aida-public/AB6AXuCEEUF3Q3nqqLWXKAkeF8BR3ms00UOHnM0afy8tidSAPD6g3jD489Ivs38SOJ-HzsjNen7xY3BTMhvWpuu7JPW8wHy79gH0SPuzGXTUDrLmN2Xg7P1NWrD59rUjTPNEtLX9CKgf1oK3MSCmO-gHXNtFVhZQbl4RlmR25Uti14Wdp0eaHls2SLGwaLBJWerkcBLZ_1P8Z6btlpeR3e9dCLURM7Olhxo4sptV0Xi0MIgyjllUI3iAl1e3j-ZDsuvsA6u8v_x9bl4r6Lhv"
+      img: "/projects/horizon.png"
     },
     { 
       id: "03.",
       title: "DENTARA", 
       desc: "Closing the gap between dental practice and technology with a high-integrity patient data ecosystem.",
-      img: "https://lh3.googleusercontent.com/aida-public/AB6AXuDvfrkF0ohKTPnNAVEL3DeCUBLn9DygwuqflIqPMWwNdShwiLxoJo-q3DR0x0DjdA1C0M8HKM0EpOcJser60D9I2v0mG2ayZBS0WcG-tF5V8OkQG17rVBnO_wxzvFLcQ2u24Bx6ge2EQjdt61j8gZMQzbESaA872TYnz7wrOFjZc0eYfpouSIGGCyKiyKoJWxG5IOhJs7JFvsv0dLfEB0oQ-_NlSSLnF8qwmzSVrPC03bP44qAu0SD5FUyYU0DbrC27r0C1YhHTsSWb"
+      img: "/projects/dentara.png"
     },
     { 
       id: "04.",
       title: "ELDERKEY", 
       desc: "Empathy-driven design meeting rigorous security standards for an inclusive digital safety net.",
-      img: "https://lh3.googleusercontent.com/aida-public/AB6AXuBXTr7YcvLMyMHgyC5ckDenfpyuuFk9KZx0QpijpghsqTM00iIxtwfGpnnUV3qf442pKiqXZ2oPwM2YL87mSykDaMTHWmSmpqiRN4OqgJfs_r1jlSux6h4aJqWUumI8TEj_ingtsGCSG8GuEztILx319gXZSM3jt_dM9NCIV73s9Q3DR5O7JSIQiKIiiBHI1rr8nBx3r5oIohCUubzkbLmaTeEIAccy7JKsrmvrddYBjpFG-wbhHdaRJuWPb5A45KyBnrpNR5DO7cbA"
+      img: "/projects/elderkey.png"
     }
   ];
 
@@ -127,6 +127,7 @@ export default function Projects() {
                         fill
                         quality={100}
                         priority
+                        unoptimized={true}
                         className="object-cover grayscale brightness-50 group-hover:grayscale-0 group-hover:brightness-100 transition-all duration-1000 group-hover:scale-105"
                       />
                    </motion.div>
